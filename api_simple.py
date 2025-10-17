76a4116f07e19196ff1cb537168b02e524476b87
Alex Maxim <97067952+nersi55@users.noreply.github.com>
2025-10-17 17:12:11 +0100

    Access via internet-2

--- api_simple.py
+++ api_simple.py
@@ -294,7 +294,8 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "api_simple:app",
-        host="127.0.0.1",
+        # host="127.0.0.1",
+        host="0.0.0.0",
         port=8002,
         reload=True,
         log_level="info"
